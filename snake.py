19bb1d1e2893221d41ad36a86b456c58ddf43f74
quintenvandenberghe <vquinten11@gmail.com>
2026-05-26 09:57:49 +0200

    scherm afsluiten

--- snake.py
+++ snake.py
@@ -1,6 +1,11 @@
-import pygame
+import pygame,sys
 pygame.init
-screen = pygame.display.set_mode((750,750))
+screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("snek")
 clock = pygame.time.Clock()
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
